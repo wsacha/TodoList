4a4b7488b6be2369d318e30148344c09658dc46c
wsacha <witorek333@gmail.com>
2020-02-06 18:20:08 +0100

    Updates

---
+++ precache-manifest.c712b2a8e4afd446f00bb10a5a690605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be762f0322118e980079df4ac8dadd6",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "43050e8dd47f89d9f42e",
+    "url": "/TodoList/static/css/main.fd5217a4.chunk.css"
+  },
+  {
+    "revision": "aac307f3f6e6aba58702",
+    "url": "/TodoList/static/js/2.c3ab550d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TodoList/static/js/2.c3ab550d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43050e8dd47f89d9f42e",
+    "url": "/TodoList/static/js/main.f4ebcb60.chunk.js"
+  },
+  {
+    "revision": "f252b29a44529df1f414",
+    "url": "/TodoList/static/js/runtime-main.e2728836.js"
+  }
+]);
